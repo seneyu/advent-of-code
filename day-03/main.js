586cf7437dee1d4ea6c5f87db89122e9ea2ebc5d
Stephany Ho <shohk39@gmail.com>
2024-12-05 19:10:45 -0500

    day 3 mull it over

---
+++ day-03/main.js
@@ -0,0 +1,26 @@
+// day-03 mull it over
+
+const input = await Deno.readTextFile('./input.txt');
+
+// console.log(input);
+
+// use regex to find all matches 'mul(num, num)'
+// use matchAll to find the matches
+// iterate the matches, compute and add to sum
+function mullItOver(input) {
+  const regex = /mul\((\d+),(\d+)\)/g;
+  // matchAll returns an iterator
+  const matches = input.matchAll(regex);
+
+  let sum = 0;
+
+  for (let match of matches) {
+    const x = parseInt(match[1]);
+    const y = parseInt(match[2]);
+    sum += x * y;
+  }
+
+  return sum;
+}
+
+console.log(mullItOver(input));
